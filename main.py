8bf03cf65bafaaf5661b6bee8daf1bcffe298a2f
Christopher2023 <66922510+Christopher2023@users.noreply.github.com>
2023-02-14 11:59:26 +0100

    importing data from local

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from azure.storage.blob import BlobClient, BlobBlock
+from azure.storage.blob import BlobServiceClient, BlobClient, BlobBlock
 import pandas as pd
 
 connection_string = "DefaultEndpointsProtocol=https;AccountName=datalakesprojectaccount;AccountKey=J8SEcu35jwdkdJPv3hg78WBgkoHc1hM0BEzu9vqfoNbEW/2zNqPEtQWM1FJqkkqhhcxHcWLn2AII+AStUhq1EQ==;EndpointSuffix=core.windows.net"
+container_name = "input"
 
 blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="processed", blob_name="stocks.csv")
+
+def uploadToBlobStorage(file_path,file_name):
+   blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+   blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+   with open(file_path,"rb") as data:
+      blob_client.upload_blob(data)
+      print(f"Uploaded {file_name}.")
+
+import os
+dir = "/stocks_data"
+for root, dirs, files in os.walk(dir):
+    for f in files:
+        uploadToBlobStorage(os.path.join(root, f),f)
 
 if blob.exists():
     blob.delete_blob()
