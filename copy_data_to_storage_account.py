86c01729c4705200fd8d803930d90724952b3067
williamsiewe16 <loicosiewe16@gmail.com>
2023-02-15 14:21:04 +0100

    finishing

--- copy_data_to_storage_account.py
+++ copy_data_to_storage_account.py
@@ -1,5 +1,8 @@
 from azure.storage.blob import BlobServiceClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 account_name = os.getenv('STORAGE_ACCOUNT_NAME', "")
 account_key = os.getenv('STORAGE_ACCOUNT_KEY', "")
@@ -9,12 +12,17 @@ container_name = "input"
 def uploadToBlobStorage(file_path,file_name):
    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+
+   if blob_client.exists():
+      blob_client.delete_blob()
+
    with open(file_path,"rb") as data:
       blob_client.upload_blob(data)
       print(f"Uploaded {file_name}.")
 
-import os
-dir = "/Users/nealfokam/Documents/M2/Datalake/TP1/stocks_data"
+
+dir = "./stocks_data"
+os.system(f"cp main.py {dir}")
 for root, dirs, files in os.walk(dir):
     for f in files:
         uploadToBlobStorage(os.path.join(root, f),f)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from azure.storage.blob import BlobServiceClient, BlobClient, BlobBlock
 import pandas as pd
 
-connection_string = "DefaultEndpointsProtocol=https;AccountName=datalakesprojectaccount;AccountKey=J8SEcu35jwdkdJPv3hg78WBgkoHc1hM0BEzu9vqfoNbEW/2zNqPEtQWM1FJqkkqhhcxHcWLn2AII+AStUhq1EQ==;EndpointSuffix=core.windows.net"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=datalakesprojectaccount;AccountKey=eHUagFYoTJhe8sooRNUaHcSKoKt8YpYXHrrodcBDD7SHwI7ehyBLOZclFznR0JWdsSq/3L5UZJJu+ASt6le6cg==;EndpointSuffix=core.windows.net"
 
 blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="processed", blob_name="stocks.csv")
 
